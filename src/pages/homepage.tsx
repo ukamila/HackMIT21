170104638e6134db219f97c188e6519853145dc7
ukamila <ukamila@bu.edu>
2021-09-18 17:58:00 -0400

    created some folders and a file for the homepage

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+interface Props {}
+
+export const HomePage: React.FC<Props> = () => {
+    return (
+        <body>
+            <title>Homepage</title>
+            <h1> The events homepage </h1>
+        </body>
+    );
+};
